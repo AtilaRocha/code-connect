ff0cfd33ddb6fb038c3dff91815e3b3055ba63f2
AtilaRocha <atila.vini@hotmail.com>
2025-02-12 08:55:41 -0300

    aula 5

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,20 @@
-import "./globals.css";
+import { Aside } from '@/components/Aside'
+import './globals.css'
 
 export const metadata = {
-  title: "Code Connect",
-  description: "Umda rede social para devs!",
-};
+  title: 'Code Connect',
+  description: 'Uma rede social para devs!',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body className>{children}</body>
+      <body>
+        <div className='app-container'>
+          <Aside />
+          {children}
+        </div>
+      </body>
     </html>
-  );
+  )
 }
